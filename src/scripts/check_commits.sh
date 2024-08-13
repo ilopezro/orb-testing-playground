da00a225bbc701831950a8a5a11a17b3e5718328
Isai Lopez Rodas <40477553+ilopezro@users.noreply.github.com>
2024-08-13 15:41:01 -0700

    fix: more typos

--- src/scripts/check_commits.sh
+++ src/scripts/check_commits.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eo pipefail
 
-echo "Enforcing separate commits in the directory: $PATH_TO_ENFORCE"
+echo "Enforcing separate commits in the directory: $PATH_TO_CHECK"
 
 # Get the current branch name
 current_branch=$(git rev-parse --abbrev-ref HEAD)
@@ -21,9 +21,9 @@ for commit in $commits; do
   # Get the list of files changed in this commit
   changed_files=$(git diff-tree --no-commit-id --name-only -r "$commit")
   # Check if any files from the directory are in this commit
-  files=$(echo "$changed_files" | grep "${PATH_TO_ENFORCE}")
+  files=$(echo "$changed_files" | grep "${PATH_TO_CHECK}")
   # Check if there are other files in this commit
-  other_files=$(echo "$changed_files" | grep -v "${PATH_TO_ENFORCE}")
+  other_files=$(echo "$changed_files" | grep -v "${PATH_TO_CHECK}")
   # If both files and other_files are not empty, fail the build
   if [[ -n "$files" && -n "$other_files" ]]; then
     echo "Error: Commit $commit is not clean."
