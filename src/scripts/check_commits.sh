cd27e026584bbdd0a1b34d40e97ea3958e89c6a4
Isai Lopez Rodas <40477553+ilopezro@users.noreply.github.com>
2024-08-13 16:24:31 -0700

    chore: more logging (#9)

--- src/scripts/check_commits.sh
+++ src/scripts/check_commits.sh
@@ -5,7 +5,6 @@ echo "Enforcing separate commits in the directory: $PATH_TO_CHECK"
 
 # Get the current branch name
 current_branch=$(git rev-parse --abbrev-ref HEAD)
-# Fast-pass if the branch is 'merging'
 if [[ "$current_branch" == "merging" ]]; then
   echo "We are borsing. Fast-passing the check."
   exit 0
@@ -14,13 +13,10 @@ fi
 echo "Checking commits in branch $current_branch..."
 
 commits=$(git rev-list master..HEAD)
-
-echo "Beginning to check commits..."
 echo "Commits to check: $commits"
 
 for commit in $commits; do
   echo "Checking commit $commit..."
-
   changed_files=$(git diff-tree --no-commit-id --name-only -r "$commit")
   echo "Changed files: $changed_files"
 
@@ -28,26 +24,19 @@ for commit in $commits; do
     continue
   fi
 
-  files=$(echo "$changed_files" | grep -F "${PATH_TO_CHECK}")
-  echo "calculated files $files"
-  other_files=$(echo "$changed_files" | grep -v -F "${PATH_TO_CHECK}")
-  echo "calculated other files $other_files"
+  # Match files in the specific path using ERE
+  files=$(echo "$changed_files" | grep -E "${PATH_TO_CHECK}")
+  echo "Matched restricted files: $files"
 
-  if [[ -n $files ]]; then
-    echo "Restricted directory files found in commit $commit."
-  fi
-
-  if [[ -n $other_files ]]; then
-    echo "Other files found in commit $commit."
-  fi
+  # Match files that are not in the specific path using ERE
+  other_files=$(echo "$changed_files" | grep -v -E "${PATH_TO_CHECK}")
+  echo "Matched other files: $other_files"
 
   if [[ -n "$files" && -n "$other_files" ]]; then
     echo "Error: Commit $commit is not clean."
-    echo "Restricted directory files found batched together with other files. Please separate them into different commits."
     exit 1
   fi
 done
-# If all commits are clean, exit successfully
 
 echo "All commits are clean. Exiting successfully."
 exit 0
